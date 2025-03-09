b364b839cd4326653c7a8d01b459d02440e126c0
Juan Visney <jvcode23@gmail.com>
2025-03-09 11:56:03 -0400

    correccion ruta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { connectDB } = require("./config/data-source");
-const invitacionRoutes = require("./routes/invitacionRoutes");
+const rutas = require("./routes/index");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 connectDB()
   .then(() => {
-    app.use("/api", invitacionRoutes);
+    app.use("/api", rutas);
 
     // Iniciar el servidor
     app.listen(port, () => {
